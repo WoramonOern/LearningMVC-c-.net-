e4bed62d4bc87621da77016e7091ae2407dadb1f
WoramonOern <woramon.chareonsuk@gmail.com>
2016-10-21 15:22:42 -0700

    Edit Exercise 2 Section 3 followed by the solution.

--- Vidly2/Models/MembershipType.cs
+++ Vidly2/Models/MembershipType.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vidly2.Models
 {
     public class MembershipType
     {
         public byte Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public short SignUpFee { get; set; }
         public byte DurationInMonths { get; set; }
