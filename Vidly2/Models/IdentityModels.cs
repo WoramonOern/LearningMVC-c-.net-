e34cc6fbdb6632f25c6d41d12f3f83629df9477a
WoramonOern <woramon.chareonsuk@gmail.com>
2016-10-21 16:42:51 -0700

    Do Exercise 3 Section 3 -not finish

--- Vidly2/Models/IdentityModels.cs
+++ Vidly2/Models/IdentityModels.cs
@@ -22,6 +22,7 @@ namespace Vidly2.Models
     {
         // data set represent data of customer table
         public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
