a1b6372bbd708fc4238b496ac9db5e3a309fbbe9
WoramonOern <woramon.chareonsuk@gmail.com>
2016-11-01 12:24:48 -0700

    Validate Anti Forgery Token

--- Vidly2/Controllers/CustomersController.cs
+++ Vidly2/Controllers/CustomersController.cs
@@ -35,6 +35,7 @@ namespace Vidly2.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(CustomerMembershipTypeViewModel postCustomer)//(CustomerMembershipTypeViewModel viewModel) // customer class is easy to pass all of data
         {
             var customer = new Customer
