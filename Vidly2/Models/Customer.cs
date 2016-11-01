dcc3fc97376e9474335ce94b13efe0567e9ae128
WoramonOern <woramon.chareonsuk@gmail.com>
2016-11-01 10:24:21 -0700

    Data Annotations with Validation

--- Vidly2/Models/Customer.cs
+++ Vidly2/Models/Customer.cs
@@ -9,7 +9,7 @@ namespace Vidly2.Models
     public class Customer
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Please enter customer's name.")]
         [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
