37d0a8d1b7b9fd42ae62b2aa0c7fd196596f3090
WoramonOern <woramon.chareonsuk@gmail.com>
2016-11-02 10:41:52 -0700

    DTOs

---
+++ Vidly2/DTOs/CustomerDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly2.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly2.DTOs
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter customer's name.")]
+        [StringLength(255)]
+        public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public byte MembershipTypeId { get; set; }
+        [Min18YearsIfAMember]
+        public DateTime? BirthDate { get; set; }
+    }
+}
