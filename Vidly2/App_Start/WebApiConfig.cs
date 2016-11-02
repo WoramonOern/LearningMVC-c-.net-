dc7d538e1caa7beb38261ea6c889e336b3a038af
WoramonOern <woramon.chareonsuk@gmail.com>
2016-11-02 11:51:43 -0700

    Camel Casing

--- Vidly2/App_Start/WebApiConfig.cs
+++ Vidly2/App_Start/WebApiConfig.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace Vidly2
 {
@@ -9,6 +11,10 @@ namespace Vidly2
     {
         public static void Register(HttpConfiguration config)
         {
+            var setting = config.Formatters.JsonFormatter.SerializerSettings;
+            setting.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            setting.Formatting = Formatting.Indented;
+
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
